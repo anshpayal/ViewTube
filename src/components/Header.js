cd4ea8a9023946eb09e9dd635056b048cd007bd8
Ansh Payal <anshpayal754@gmail.com>
2024-01-31 20:36:52 +0530

    Added caching in search

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,17 @@ import Logo from "../Logo.png";
 import { FiSearch } from "react-icons/fi";
 import { TfiAlignJustify } from "react-icons/tfi";
 import { FaCircleUser } from "react-icons/fa6";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { hideMenu } from "../redux store/appSlice";
 import { YOUTUBE_SEARCHSUGGESTIONS_API } from "../utils/Links";
+import { cacheResults } from "../redux store/searchSlice";
 
 const Header = () => {
   const [searchValue, setSearchValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestion, setShowSuggestion] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
 
   const dispatch = useDispatch();
 
@@ -28,13 +31,28 @@ const Header = () => {
   const searchSuggestions = async () => {
     const data = await fetch(YOUTUBE_SEARCHSUGGESTIONS_API + searchValue);
     const json = await data.json();
-    console.log(json[1]);
+    //console.log(json[1]);
     setSuggestions(json[1]);
+    //update cache
+    dispatch(cacheResults({
+      [searchValue]: json[1],
+    }))
   };
+  /*
+    searchCache = {
+      "iphone" : [iphone 11, iphone 12]
+    }
+    searchValue= iphone
+  */
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      searchSuggestions();
+      if (searchCache[searchValue]) {
+        setSuggestions(searchCache[searchValue]);
+      } else {
+        searchSuggestions();
+      }
     }, 200);
     const handleScroll = () => {
       setShowSuggestion(false);
@@ -42,9 +60,8 @@ const Header = () => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       clearTimeout(timer);
-      window.removeEventListener("scroll", handleScroll);
     };
-  }, [searchValue]);
+  }, [searchValue, searchCache]);
 
   return (
     <div className=" flex items-center justify-between border border-b-gray-300 shadow-lg">
@@ -74,9 +91,12 @@ const Header = () => {
         <div className="my-1 absolute top-full left-0 bg-white shadow-xl w-11/12 rounded-2xl">
           <ul>
             {showSuggestion &&
-              suggestions.map((suggestion,index) => {
+              suggestions.map((suggestion, index) => {
                 return (
-                  <li key={index} className="p-2 text-lg flex items-center hover:bg-gray-200">
+                  <li
+                    key={index}
+                    className="p-2 text-lg flex items-center hover:bg-gray-200"
+                  >
                     {" "}
                     <span className="mr-2">
                       <FiSearch />
